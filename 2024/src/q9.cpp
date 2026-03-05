addf4a595271f05fe4e621bebdbf719f80ad12f4
Zhi Liang <zhikang8@hotmail.co.uk>
2026-03-05 02:26:05 +0000

    Skeleton files

---
+++ 2024/src/q9.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <vector>
+
+int main() {
+  std::ifstream input_1("./data/q__1.txt");
+  std::ifstream input_2("./data/q__2.txt");
+  std::ifstream input_3("./data/q__3.txt");
+  std::string line;
+  
+  std::cout << "Answer to part 1: " << std::endl;
+  std::cout << "Answer to part 2: " << std::endl;
+  std::cout << "Answer to part 3: " << std::endl;
+  return 0;
+}
